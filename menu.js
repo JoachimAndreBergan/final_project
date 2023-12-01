d2e6112f0013ebb269a4abc6d50962b513638627
JoachimAndreBergan <jobe016@student.kristiania.no>
2023-12-01 19:32:20 +0100

    added function addClass

--- menu.js
+++ menu.js
@@ -7,3 +7,16 @@ function filterObjects(c){
         if(x[i].className.indexOf(c) > -1) addClass(x[i], "show")
     }
 }
+
+function addClass(element, name){
+    let i, arr1, arr2;
+    arr1=element.className.split("");
+    arr2=name.split("")
+    for (i=0; i < arr2.length; i++){
+        if (arr1.indexOf(arr2[i]) == -1 ){
+            element.className += "" + arr2[i];
+        }
+    }
+
+
+}
