5bbbae1608983abdcdf55c881bbef13415d8da20
JoachimAndreBergan <jobe016@student.kristiania.no>
2024-05-30 15:23:09 +0200

    started js for login page. gotten elements by id

---
+++ login.js
@@ -0,0 +1,8 @@
+const email = document.getElementById('email'),
+password = document.getElementById('password'),
+emailErr = document.getElementById('emailErr'),
+passErr = document.getElementById('passErr'),
+LogInArea = document.getElementById('LogInArea'),
+login = document.getElementById('login');
+
+
